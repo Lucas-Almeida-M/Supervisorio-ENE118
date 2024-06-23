163c4e7ce1007b052af750c42c7e5b6a9c3cc6a4
lucaass95 <lucas20141956@gmail.com>
2024-06-23 00:26:47 -0300

    functioning until termometer in 1:41

--- supervisorio/main.py
+++ supervisorio/main.py
@@ -1,1 +1,31 @@
-print("Hello world")
+from kivy.app import App 
+from mainwidget import MainWidget
+from kivy.lang.builder import Builder
+
+
+class MainApp(App):
+
+    def build(self):
+
+        self._widget = MainWidget(scan_time = 1000, server_ip = '127.0.0.1', server_port = 502,
+        modbus_addrs = {
+            'fornalha' : 1000,
+            'gas_ref' : 1001,
+            'gasolina' : 1002,
+            'nafta' : 1003,
+            'querosene' : 1004,
+            'diesel' : 1005,
+            'oleo_lub' : 1006,
+            'oleo_comb' : 1007,
+            'residuos' : 1008
+        }
+                                  
+                                  
+        )
+        return self._widget
+    
+
+if __name__ == '__main__':
+    Builder.load_string(open("mainwidget.kv", encoding="utf-8").read(), rulesonly = True)
+    Builder.load_string(open("popups.kv", encoding="utf-8").read(), rulesonly = True)
+    MainApp().run()
---
+++ supervisorio/mainwidget.py
@@ -0,0 +1,105 @@
+from kivy.uix.boxlayout import BoxLayout
+from popups import ModbusPopup, ScanPopup
+from pyModbusTCP.client import ModbusClient
+from kivy.core.window import Window
+from threading import Thread
+from time import sleep
+from datetime import datetime
+import random
+
+
+
+class MainWidget (BoxLayout):
+    """
+    Widget principal da aplicação
+    """
+    _updateThread = None
+    _updateWidgets = True
+    _tags = {}
+
+    def __init__(self, **kwargs):
+        """
+        construtor do widget principal
+        """
+        super().__init__()
+        self._scan_time = kwargs.get('scan_time')
+        self._serverIP = kwargs.get('server_ip')
+        self._serverPort = kwargs.get('server_port')
+        self._modbusPopup = ModbusPopup(self._serverIP, self._serverPort)
+        self._scanPopup = ScanPopup(scantime = self._scan_time)
+        self._modbusClient = ModbusClient(host = self._serverIP, port = self._serverPort)
+        self._meas = {}
+        self._meas ['timestamp'] = None
+        self._meas ['values'] = {}
+        for key,value in kwargs.get('modbus_addrs').items():
+            if key == 'fornalha':
+                plot_color = (1,0,0,1)
+            else:
+                plot_color = (random.random(),random.random(),random.random(),1)
+            self._tags[key] = {'addr' : value, 'color' : plot_color}
+
+        
+
+    def startDataRead(self, ip, port):
+        """
+        Método utilizado para configuração do IP e porta do servidor Modbus e
+        inicializar uma thread para a leitura dos dados e atualização da interface
+        gráfica
+        """
+
+        self._serverIP = ip
+        self._serverPort = port
+        self._modbusClient.host = self._serverIP
+        self._modbusClient.port = self._serverPort
+        try:
+            Window.set_system_cursor("wait")
+            self._modbusClient.open()
+            Window.set_system_cursor("arrow")
+            if self._modbusClient.is_open:
+                self._updateThread = Thread(target=self.updater)
+                self._updateThread.start()
+                self.ids.img_con.source = "imgs/conectado.png"
+                self._modbusPopup.dismiss()
+            else:
+                self._modbusPopup.setInfo("Falha na conexão com o servidor")
+
+        except Exception as e:
+            print("Erro: ", e.args)
+
+    def updater(self):
+        """
+        Método que invoca as rotinas de leitura dos dados, atualização da 
+        interface e inserção dos dados no Banco de Dados
+        """
+
+        try:
+            while self._updateWidgets:
+                self.readData()
+                self.updateGUI()
+                #inserir dados no BD
+                sleep(self._scan_time/1000) #ms
+        except Exception as e:
+            self._modbusClient.close()
+            print("Erro: ", e.args)
+
+    def readData(self):
+        '''
+        Metodo para leitura dos dados por meio do protocolo MODBUS
+        '''
+        self._meas ['timestamp'] = datetime.now()
+        for key, value in self._tags.items():
+            self._meas['values'][key] = self._modbusClient.read_holding_registers(value['addr'],1)[0]
+        
+    def updateGUI(self):
+        """
+        Metodo para atualizacao da interface grafica a partir dos dados lidos
+        """
+
+        #atualizacao dos labels das temperaturas
+        for key,value in self._tags.items():
+            self.ids[key].text = str(self._meas['values'][key]) + ' C'
+        #Atualizar nivel do termometro 
+        self.ids.lb_temp.size = (self.ids.lb_temp.size[0], self._meas['values']['fornalha']/450*self.ids.termometro.size[1])
+
+
+
